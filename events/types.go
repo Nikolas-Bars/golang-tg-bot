25effb84cf1ececd6c1c050cae5b99d1dcad4dd4
Николай Парсолов <docum.magic0@gmail.com>
2025-08-20 09:13:36 +0300

    events

---
+++ events/types.go
@@ -0,0 +1,23 @@
+package events
+// все что находится в пакете events сможет работать с 
+// любым мессенджером
+type Fetcher interface {
+	Fetch(limit int) ([]Event, error)
+}
+
+type Processor interface {
+	Process(e Event) error
+}
+
+type Type int
+
+const (
+	// iota - присваивает первой константе значение 0, а далее увеличивает на 1
+	Unknown Type = iota
+	Message // автоматом будет 1 из-за iota
+)
+
+type Event struct {
+	Type Type
+	Text string
+}
