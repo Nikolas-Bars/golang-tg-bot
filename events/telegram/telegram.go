0a9d8480ce9397a17dda23d3bcd7fbb230c28cfa
nparsolov <docum.magic0@gmail.com>
2025-08-22 21:56:56 +0300

    lesson 4

--- events/telegram/telegram.go
+++ events/telegram/telegram.go
@@ -1,1 +1,13 @@
 package telegram
+
+import "golang-bot/clients/telegram"
+
+type ProcessorStruct struct {
+	tg *telegram.Client
+	offset int
+
+}
+
+func New(client *telegram.Client, storage int) {
+
+}
